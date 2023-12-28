72b3b3f7fe0acb69bafc1964f43cdd9d9c4b4237
ycb0366 <145772587+ycb0366@users.noreply.github.com>
2023-12-28 23:04:30 +0800

    LGBMRegressor.py

--- LGBMRegressor.py
+++ LGBMRegressor.py
@@ -10,30 +10,30 @@ from matplotlib import pyplot as plt
 
 
 def nb_news():
-    # (1)获取数据
-    data = pd.read_csv("D:\\a_大二课程\\人工智能\\5G用户预测\\train.csv")
+    #Get the data
+    data = pd.read_csv("train.csv")
     target = data['target']
     data = data.drop(['id', 'target'], axis=1)
-    # 找到num开头的列
+    # Find columns starting with 'num_'
     low_cardinality_columns = [col for col in data.columns if col.startswith('num_')]
 
-    # 计算这些列的平均值，创建一个新的特征
+    #Calculate the mean of these columns to create a new feature
     data['low_cardinality_mean'] = data[low_cardinality_columns].mean(axis=1)
 
-    # 选择连续变量和新的特征
+    #Select continuous variables and the new feature
     data_continuous = data[low_cardinality_columns + ['low_cardinality_mean']]
 
-    # 连接其他数值型特征
+    # Combine other numerical features
     other_numerical_columns = [col for col in data.columns if col.startswith('cat_') ]
     data_other_numerical = data[other_numerical_columns]
 
-    # 合并数据
+    # Merge the data
     data_processed = pd.concat([data_continuous, data_other_numerical], axis=1)
-    # 初始化随机森林分类器
+    # Initialize the LGBMRegressor
     rf_classifier = LGBMRegressor()
-    # 拟合模型
+    # Fit the model
     rf_classifier.fit(data_processed, target)
-    # 获取特征重要性
+    # Get feature importances
     feature_importances = rf_classifier.feature_importances_
     feature_names = data_processed.columns
 
@@ -45,7 +45,7 @@ def nb_news():
     plt.xticks(rotation=45, ha="right")
     plt.tight_layout()
     plt.show()
-    # (2)划分数据集
+    #Split the dataset
     x_train, x_test, y_train, y_test = train_test_split(data_processed, target, test_size=0.3)
 
     
@@ -53,11 +53,11 @@ def nb_news():
     estimator = LGBMRegressor()
     estimator.fit(x_train, y_train)
 
-    # （5）模型评估
+    #Model evaluation
     y_predict = estimator.predict(x_test)
 
-    print("预测结果:\n", y_predict)
-    print("预测正确率:\n", y_test == y_predict)
+    print("Predictions:\n", y_predict)
+    print("Accuracy:\n", y_test == y_predict)
 
     roc_auc = roc_auc_score(y_test, y_predict)
     print("roc_auc:", roc_auc)
