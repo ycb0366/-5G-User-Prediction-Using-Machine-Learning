56d1adeeba335033a7c5c16871fbf8f2c381d077
ycb0366 <145772587+ycb0366@users.noreply.github.com>
2023-12-28 23:01:26 +0800

    GaussianNB.py

--- GaussianNB.py
+++ GaussianNB.py
@@ -8,41 +8,41 @@ from imblearn.over_sampling import SMOTE
 
 
 def nb_news():
-    # (1)获取数据
-    data = pd.read_csv("D:\\a_大二课程\\人工智能\\5G用户预测\\train.csv")
+    # (1)Get the data
+    data = pd.read_csv("train.csv")
     target = data['target']
     data = data.drop('target', axis=1)
 
-    # 找到每列不一样的数据少于100个的列
+    #Find columns where the number of unique values is less than 100
     low_cardinality_columns = [col for col in data.columns if data[col].nunique() < 100]
     data['low_cardinality_mean'] = data[low_cardinality_columns].mean(axis=1)
 
-    # 选择连续变量和新的特征
+    # Select continuous variables and new features
     data_continuous = data[low_cardinality_columns + ['low_cardinality_mean']]
     other_numerical_columns = [col for col in data.columns if col.startswith('num_') or col.startswith('cat_')]
     data_other_numerical = data[other_numerical_columns]
 
-    # 数据变换
+    #  Data transformation
     transformer = PowerTransformer()
     data_other_numerical_transformed = transformer.fit_transform(data_other_numerical)
 
-    # 合并数据
+    # Combine the data
     data_processed = pd.concat(
         [data_continuous, pd.DataFrame(data_other_numerical_transformed, columns=other_numerical_columns)], axis=1)
 
-    # 划分数据集
+    # Split the dataset
     x_train, x_test, y_train, y_test = train_test_split(data_processed, target, test_size=0.1, random_state=42)
 
 
-    # 高斯贝叶斯算法
+    # Gaussian Naive Bayes algorithm
     estimator = GaussianNB()
     estimator.fit(x_train, y_train)
 
-    # 模型评估
+    # Model evaluation
     y_predict = estimator.predict(x_test)
     roc_auc = roc_auc_score(y_test, y_predict)
 
-    print("预测结果:\n", y_predict)
+    print("Predictions:\n", y_predict)
     print("roc_auc:", roc_auc)
 
 
